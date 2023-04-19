c29b6fe762b105f6d5ccac527f507d16dac24bc5
Adelina515 <adelgodlevska@gmail.com>
2023-04-20 00:23:35 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import './css/styles.css';
 import { fetchCountries } from "./fetchCountries.js";
+import debounce from "lodash.debounce";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const DEBOUNCE_DELAY = 300;
-
-const result = fetchCountries();
-console.log(result);
-const refsInput = document.getElementById('search-box');
-
-refsInput.addEventListener('input', _.debounce(handleInput, 300));
-
-function handleInput() {
-    
+const refs = {
+ input: document.getElementById('search-box'),
+ countryList: document.querySelector('.country-list'),
+ countryInfo: document.querySelector('.country-info'),
 }
 
-/*_ . debounce (calculateLayout ,  150 ) */
+
+const result = fetchCountries();
+
+refs.input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
+
+function handleInput(ev) {
+    const inputValue = refsInput.value.trim();
+    if (inputValue = '') {
+        refs.countryInfo.innerHTML = '';
+        refs.countryList.innerHTML = '';
+        return;
+    } else if ( result > 10) {
+        Notify.warning("Too many matches found. Please enter a more specific name.");
+    }else if ( result < 10 && result > 2) {
+        return 
+    }
+
+}
+
