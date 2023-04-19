3910ec672d9188a5771c9ccce18378c561937896
Adelina515 <117230185+Adelina515@users.noreply.github.com>
2023-04-19 20:18:26 +0000

    Deploying to gh-pages from @ Adelina515/goit-js-hw-10@7a28a0616dc16304c1a06c8d9b6fc6e6af3f4455 🚀

---
+++ index.4998c53b.js
@@ -0,0 +1,2 @@
+!function(){var n,e=fetch("".concat("https://restcountries.eu/rest/v2/name").concat(n,"?fields=name,flags,capital,population,languages")).then((function(n){return n.json()})).then((function(n){return console.log(n)}));console.log(e),document.getElementById("search-box").addEventListener("input",_.debounce((function(){}),300))}();
+//# sourceMappingURL=index.4998c53b.js.map
