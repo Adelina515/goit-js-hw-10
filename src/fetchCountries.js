72879fa18b37aaec0fe06f7faaa390f72a66aca2
Adelina515 <adelgodlevska@gmail.com>
2023-04-23 14:40:52 +0300

    add then

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,9 @@
 const ENDPOINT = "https://restcountries.eu/rest/v2/name";
 
  function fetchCountries(name) {
-    return fetch(`${ENDPOINT}${name}?fields=name,flags,capital,population,languages`)
-    .then((data) => data.json())
-    .then((data) => console.log(data))
+    return fetch(`${ENDPOINT}${name}?fields=name,flags.svg,capital,population,languages`)
+        .then((data) => data.json())
+        .then((data) => data[0])
+        .then((name) => console.log(name))
 }
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -10,22 +10,39 @@ const refs = {
  countryInfo: document.querySelector('.country-info'),
 }
 
-
 const result = fetchCountries();
 
 refs.input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
 function handleInput(ev) {
-    const inputValue = refsInput.value.trim();
-    if (inputValue = '') {
-        refs.countryInfo.innerHTML = '';
+    const inputValue = ev.currentTarget.value.trim();
+    fetchCountries(inputValue);
+    if (inputValue === 0) {
         refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
         return;
-    } else if ( result > 10) {
+    } else if ( result.length > 10) {
         Notify.warning("Too many matches found. Please enter a more specific name.");
-    }else if ( result < 10 && result > 2) {
+    }else if ( result.length < 10 && result > 2) {
+        return refs.countryList.insertAdjacentHTML("beforeend", ) = '';
+             refs.countryInfo.insertAdjacentHTML = '';
+                
+    }else if ( result.length === 1) {
         return 
     }
 
 }
+function createCountryInfo(country) {
+     
+ } 
+
+/*const list = document.querySelector(".list");
+
+const newTechnologies = ["React", "TypeScript", "Node.js"];
+const markup = newTechnologies
+  .map((technology) => `<li class="list-item new">${technology}</li>`)
+  .join("");
+
+list.insertAdjacentHTML("beforeend", markup);
+list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>"); */
 
