0e3daad2e387bed909dd9a7a69081a2fab65e405
Adelina515 <adelgodlevska@gmail.com>
2023-04-23 18:50:47 +0300

    add html

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,14 @@
-const ENDPOINT = "https://restcountries.eu/rest/v2/name";
 
- function fetchCountries(name) {
+
+function fetchCountries(name) {
+     const ENDPOINT = "https://restcountries.eu/rest/v2/name";
     return fetch(`${ENDPOINT}${name}?fields=name,flags.svg,capital,population,languages`)
-        .then((data) => data.json())
-        .then((data) => data[0])
+        .then((data) => {
+            if (!data.ok) {
+			throw new Error(data.status);
+		}
+		return data.json()
+        })      
         
 }
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -16,14 +16,9 @@ refs.input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
 function handleInput(ev) {
     const inputValue = refs.input.value.trim();
-    fetchCountries(inputValue).then(({ name }) => {
-        console.log(name);
-        if (name.length === 0) throw new Error('No data!')
-        
-        name.reduce((acc) => {
-
-        })
-
+    fetchCountries(inputValue).then(({ names }) => {
+        console.log(names);
+        if (names.length === 0) throw new Error('No data!')
     }).catch(onError);
     if (inputValue === 0) {
         refs.countryList.innerHTML = '';
@@ -32,32 +27,38 @@ function handleInput(ev) {
     } else if ( result.length > 10) {
         Notify.warning("Too many matches found. Please enter a more specific name.");
     }else if ( result.length < 10 && result > 2) {
-        // return refs.countryList.insertAdjacentHTML("beforeend", ) = '';
-        //      refs.countryInfo.insertAdjacentHTML = '';
+        return refs.countryList.insertAdjacentHTML("beforeend", createMarkupList());
+        
       /*Якщо бекенд повернув від 2-х до 10-и країн, під тестовим полем відображається 
       список знайдених країн.
        Кожен елемент списку складається з прапора та назви країни. */          
     }else if ( result.length === 1) {
-        return 
+        return refs.countryInfo.insertAdjacentHTML("beforeend", createMarkup());
         /*Якщо результат запиту - це масив з однією країною, в інтерфейсі відображається 
         розмітка картки з даними про країну: прапор, назва, столиця, населення і мови. */
     }
 
 }
+
+function createMarkupList({name, flags}) {
+    return `<img src = ${flags.svg} alt='flags of ${name.official}' width=60 height=40/>`
+}
+
+function createMarkup({name, flags, capital, population, languages}) {
+
+    return`<div class="country-inform">
+    <h2 class="country-name">${name.official}</h2>
+    <img src = ${flags.svg} alt='flags of ${name.official}' width=60 height=40/>
+    <p class="country-capital"${capital}</p>
+    <p class="country-population"${population}</p>
+    <p class="country-languages"${languages}</p>
+    </div> `
+}
 function onError(err) {
     console.error(err)
+    Notify.warning("Oops, there is no country with that name");
 }
-function createCountryInfo(country) {
-     
- } 
+ 
 
-/*const list = document.querySelector(".list");
 
-const newTechnologies = ["React", "TypeScript", "Node.js"];
-const markup = newTechnologies
-  .map((technology) => `<li class="list-item new">${technology}</li>`)
-  .join("");
-
-list.insertAdjacentHTML("beforeend", markup);
-list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>"); */
 
