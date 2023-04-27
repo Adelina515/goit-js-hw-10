3e9a7b0ae3b1e7e6f49fa0639c0fd1e4ea915d43
Adelina515 <adelgodlevska@gmail.com>
2023-04-27 08:15:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,21 +16,17 @@ refs.input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
 function handleInput(ev) {
     const inputValue = refs.input.value.trim();
-   const result = fetchCountries(inputValue).then(response => {
-        console.log(response);
-       if (response.length === 0) {
+   const result = fetchCountries(inputValue).then(country => {
+        console.log(country);
+       if (country.length === 0) {
            throw new Error('No data!')
        } else if ( result.length > 10) {
         Notify.warning("Too many matches found. Please enter a more specific name.");
     }else if ( result.length < 10 && result.length > 2) {
         return refs.countryList.insertAdjacentHTML("beforeend", createMarkupList(name, flags));
-      /*
-       ви намагаєтесь отримати властивість length від змінної result, яка є об'єктом Promise, 
-       а не масивом, тому у вас виводить андефайнд. Вам потрібно використ. даний масив у ф-ції 
-       handleInput після того, як він буде отриманий з fetchCountries. Для цього вам потрібно 
-       додати код обробки результату запиту в блоці then відповідно до ваших потреб */          
+               
     }else if ( result.length === 1) {
-        return refs.countryInfo.insertAdjacentHTML("beforeend", createMarkup(name, flags, capital, population, languages));
+        return refs.countryInfo.insertAdjacentHTML("beforeend", createMarkup(country));
         /*Якщо результат запиту - це масив з однією країною, в інтерфейсі відображається 
         розмітка картки з даними про країну: прапор, назва, столиця, населення і мови. */
     } else if (inputValue === 0) {
@@ -43,12 +39,14 @@ function handleInput(ev) {
 
 }
 
-function createMarkupList({name, flags}) {
-    return `<img src = ${flags.svg} alt='flags of ${name.official}' width=60 height=40/>`
+function createMarkupList({ name, flags }) {
+    if (name) {
+       return `<img src = ${flags.svg} alt='flags of ${name.official}' width=60 height=40/>` 
+    }
 }
 
-function createMarkup(response) {
-    return response.map(({name, flags, capital, population, languages}) =>`<div class="country-inform">
+function createMarkup(country) {
+    return country.map(({name, flags, capital, population, languages}) =>`<div class="country-inform">
     <h2 class="country-name">${name.official}name</h2>
     <img src = ${flags.svg} alt='flags of ${name.official}' width=60 height=40/>
     <p class="country-capital"><span>capital:</span>${capital}</p>
